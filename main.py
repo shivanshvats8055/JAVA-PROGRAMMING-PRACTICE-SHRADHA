640923a47785979bd88d019b81535410fce14bbf
shivanshvats8055 <122456199+shivanshvats8055@users.noreply.github.com>
2025-11-16 03:47:51 +0530

    day1

---
+++ main.py
@@ -0,0 +1,142 @@
+# if else elif statement
+# gen = input("enter the gender of the specific employee as charcter: ")
+# if gen == "M" or gen == "m":
+#     print("Good morning sir")
+# elif gen == "F" or gen == "f":
+#     print("good morning maam")
+# else:
+#     print("unspecified gender")
+
+# num = int(input("enter the number: "))
+# if num%2==0:
+#     print("even")
+# else:
+#     print("odd")
+
+# year = int(input("enter year: "))
+# if year%100 == 0 and year%400 == 0:
+#     print("its a leap year")
+# elif year%100!=0 and year%4 == 0:
+#     print("its a leap year")
+# else:
+#     print("its a normal year")
+
+# loops
+# list2 = ["apple","mango","carrot","grapes"]
+# for i in range(len(list2)):
+#     print(list2[2])
+#     break#  a = range(1,21,1)
+# for i in range(5,51,5):
+#     print(i)
+
+# a = "sheriyans"
+# for i in range(len(a)):
+#     print(a[i])
+
+# a = "shivansh"
+# for i in a:
+#     print(i,end=" ")
+
+# accept an integer and print hello world n times
+# n = int(input("please tell your number: "))
+# for i in range(n):
+#     print("hello world")
+
+#reverse for loop
+# n = int(input("enter the number: "))
+# for i in range(n, 0 ,-1):
+#     print(i)
+
+# accept an integer and print its table
+# number = int(input("enter the number: "))
+# for i in range(1,11):
+#     print(number*i)
+
+#sum upto n terms
+
+# n = int(input("enter the number: "))
+# sum = 0
+# for i in range(0,n+1):
+#     sum += i
+# print(sum)
+
+# factorial of a number
+# n = int(input("enter the number: "))
+# fact = 1
+# for i in range(1,n+1):
+#     fact *= i
+
+# print(fact)
+
+# print the sum of all even and odd numbers in a range seperately
+
+# range2 = int(input("enter the number: "))
+# sum1 = 0
+# sum2 = 0
+# for i in range(1,range2+1):
+#     if i%2==0:
+#         sum1 += i
+#     else:
+#         sum2 += i
+
+# print(sum1)
+# print(sum2)
+
+# print all the factors of a number
+# number = 66
+# for i in range(1,66+1):
+#     if number%i==0:
+#         print(i)
+
+# accept a number and check if it is a perfect number or not
+# n = int(input("enter the number you want to check: "))
+# sum1 = 0
+# for i in range(1,n):
+#     if n % i == 0:
+#         sum1 += i
+
+# if sum1 == n:
+#     print("the number is a perfect number")
+# else:
+#     print("the number is not a perfect number")
+
+# check whether the number is prime or not
+# n = int(input("check your number is prime or not: "))
+# count = 0
+# for i in range(1,n+1):
+#     if n%i == 0:
+#         count += 1
+
+# if count == 2:
+#     print(f"{n} is a prime number")
+# else:
+#     print(f"{n} is not a prime number")
+
+# a = "SHIVANSH"
+# b = ""
+# for i in range(len(a)-1,-1,-1):
+#     b = b+a[i]
+
+# print(b)     
+
+# check string is palindrome or not
+
+# a = input("enter the string for checking palindrome: ")
+# b = ""
+# for i in range(len(a)-1,-1,-1):
+#     b += a[i]
+
+# if b == a:
+#     print("the string is palindrome ")
+# else:
+#     print("the string is not a palindrome")
+
+# to determine the number of digits alphabet and special character in a string using string methods
+
+
+ 
+ 
+
+
+
+
---
+++ shivansh.java
@@ -0,0 +1,428 @@
+// class FirstClass {
+//     public static void main(String args[]){
+//         System.out.println("hello java");
+//     }
+// }
+
+// class FirstClass {
+//     public static void main(String[] args){
+//         System.out.println("hello world");
+        
+//     }
+// }
+import java.util.*;
+// public class conditions{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         int age = sc.nextInt();
+
+//         if(age>18){
+//             System.out.println("Adult");
+//         }
+//         else{
+//             System.out.println("Not adult");
+//         }
+
+        
+//     }
+// }
+
+// public class conditions{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         int x = sc.nextInt();
+
+//         if(x%2==0){
+//             System.out.println("x is an even number");
+//         }
+//         else{
+//             System.out.println("x is odd");
+//         }
+//     }
+// }
+
+// public class conditions{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
+
+//         if(a > b){
+//             System.out.println("greater");
+        
+//         }
+//         else if(a < b){
+//             System.out.println("smaller");
+//         }
+//         else{
+//             System.out.println("equal");
+//         }
+//     }
+
+// }
+
+// public class button{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         int button = sc.nextInt();
+
+//         if(button==1){
+//             System.out.println("Hello");
+//         }
+//         else if(button==2){
+//             System.out.println("Namaste");
+//         }
+//         else if(button==3){
+//             System.out.println("Bonjour");
+//         }
+//         else{
+//             System.out.println("invalid button");
+//         }
+//     }
+// }
+
+// loops in java 
+// public class loops{
+//     public static void main(String[] args){
+//         for(int i =0; i<11; i++){
+//             System.out.println(i);
+//         }
+//     }
+// }
+
+// public class loops{
+//     public static void main(String[] args){
+//         int i = 0;
+//         while (i<=10) {
+//             System.out.println(i);
+//             i++;
+//         }
+//     }
+// }
+
+// print the sum of n arbitrary numbers 
+// public class loops{
+//     public static void main(String[] args){
+//         int n = 3;
+//         int sum = 0;
+//         for(int i = 1; i<=n; i++){
+//             sum = sum+i;
+//         }
+//         System.out.println(sum);
+//     }
+// }
+
+// print the table of a number input by the user.
+// public class loops{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         int number = sc.nextInt();
+//         for(int i=1;i<=10;i++){
+//             System.out.println(i*number);
+//         }
+//     }
+// }
+
+// public class pattern{
+    
+//     public static void main(String[] args){
+//         int n = 4;
+//         int m = 5;
+//         for(int i = 1; i<=n; i++){
+//             for(int j = 1; j<=m; j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+           
+//         }
+//     }
+// }
+
+// public class patterns{
+//     public static void main(String[] args){
+//         int n = 4;
+//         int m = 5;
+//         for (int i = 1; i<=n; i++){
+//             for (int j = 1; j<=m; j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+// public class pattern{
+//     public static void main(String[] args){
+//         int n = 4;
+//         int m = 5;
+//         for(int i=1;i<=n;i++){
+//             for(int j=1; j<=m;j++){
+//                 if(i==1 || j==1 || i==n || j==m){
+//                     System.out.print("*");
+//                 }
+//                 else{
+//                     System.out.print(" ");
+//                 }
+                
+
+//             }
+//             System.out.println();
+//         }
+
+//     }
+// }
+
+// public class pattern{
+//     public static void main(String[] args){
+//         int n = 4;
+//         for(int i=1;i<=n;i++){
+//             for(int j=1;j<=i;j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+        
+//     }
+// }
+
+// public class pattern{
+//     public static void main(String[] args){
+//         int n = 4;
+//         for(int i =n;i>=1;i--){
+//             for(int j=1;j<=i;j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+// inverted half pyramid
+
+// public class pattern{
+//     public static void main(String[] args){
+//         int n = 4;
+//         for(int i=1; i<=n; i++){
+//             for(int j=1; j<=n-1 ;j++){
+//                 System.out.print(" ");
+//             }
+
+//             for(int j=1; j<=i; j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+// public class pattern{
+//     public static void main(String[] args){
+//         int n = 5;
+//         for(int i = 1;i<=n;i++){
+//             for(int j = 1;j<=i;j++){
+//                 System.out.print(j);
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+// public class pattern{
+//     public static void main(String[] args){
+//        int n = 5;
+//        for(int i =1;i<=n;i++){
+//         for(int j = 1;j<=n-1+1;j++){
+//             System.out.print(j);
+
+//         }
+//         System.out.println();
+//        } 
+//     }
+// }
+
+
+// functions and methods
+// public class pattern{
+//     public static void main(String[] args){
+//         int n = 5;
+//         for(int i = 1;i<=5;i++){
+//             for(int j = 1;j<=i;j++){
+//                 System.out.print(j+" ");
+//             }
+//             System.out.println();
+//         }
+
+//     }
+// }
+
+// public class pattern{
+//     public static void main(String[] args){
+//         int n = 5;
+//         for(int i = 1;i<=n;i++){
+//             for(int j = 1; j<=n-i+1;j++){
+//                 System.out.print(j);
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+// floyds triangle 
+// public class pattern{
+//     public static void main(String[] args){
+//         int n = 5;
+//         int number = 1;
+//         for(int i=1;i<=n;i++){
+//             for(int j=1;j<=i;j++){
+//                 System.out.print(number+" ");
+//                 number++;
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+// 0-1 triangle 
+// public class pattern{
+//     public static void main(String[] args){
+//         int n = 5;
+//         for(int i=1;i<=n;i++){
+//             for(int j=1;j<=i;j++){
+//                 int sum = i+j;
+//                 if(sum%2==0){
+//                     System.out.print("1 ");
+//                 }else{
+//                     System.out.print("0 ");
+//                 }
+
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+// advanced pattern 
+// Butterfly pattern 
+
+// Functions and methods 
+// public class Functions{
+//     public static void printMyName(String name){
+//         System.out.println(name);
+//         return;
+//     }
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         String name = sc.nextLine();
+
+//         // function call
+//         printMyName(name);
+//     }
+
+//     }
+
+// public class Functions{
+//     public static void printMyName(String name){
+//         System.out.println(name);
+//         return;
+//     }
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         String name = sc.nextLine();
+//         sc.close();
+
+//         printMyName(name);
+//     }
+// }
+
+// public class Functions{
+//     public static int sum(int a,int b){
+//         int sum = a+b;
+//         return sum;
+//     }
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
+//         sc.close();
+        
+
+//         int sum= sum( a,  b);
+//         System.out.println(sum);
+
+//     }
+
+
+    
+// }
+
+// make a function to multiply twoo numbers and return the product
+// public class Product{
+//     public static int Multiplication(int a,int b){
+//         int result = a*b;
+//         return result;
+//     }
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
+        
+
+//         int product= Multiplication(a, b);
+//         System.out.println(product);
+//     }
+// }
+
+// find the factorial of a number
+// public class Funct{
+//     public static void Factorial(int n){
+//         if(n<0){
+//             System.out.println("Invalid number");
+//             return;
+//         }
+//         int fact = 1;
+//         for(int i = n; i>=1;i--){
+//               fact = fact*i;
+//         }
+//         System.out.println(fact);
+//         return;
+            
+            
+        
+//     }
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+
+//         Factorial(n);
+//     }
+// }
+
+// public class shivansh{
+//     public static void printFactorial(int n){
+//         int factorial = 1;
+//         if(n<1){
+//             System.out.println("Invalid input");
+//             return;
+//         }
+//         for(int i = n;i>=1;i--){
+//             factorial = factorial*i; 
+//         }
+//         System.out.println(factorial);
+//         return;
+//     }
+
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+
+//         printFactorial(n);
+//     }
+// }
+
+//  enter three number from the user and 
+
+        
+
+    
+
+
