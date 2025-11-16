cd8cd68fcf5ad6391fc9adc9db1a27119925c40c
shivanshvats8055 <122456199+shivanshvats8055@users.noreply.github.com>
2025-11-17 01:44:56 +0530

    DAY2 (FUNCTION)

--- powerCalculation.java
+++ powerCalculation.java
@@ -419,7 +419,123 @@ import java.util.*;
 //     }
 // }
 
-//  enter three number from the user and 
+//  enter three number from the user and make a function to print the average .
+// public class shivansh{
+//     public static void calculateAverage(double a,double b,double c){
+//         double average = (a+b+c)/3;
+//         System.out.println(average);
+//         return;
+//     }
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         double a = sc.nextInt();
+//         double b = sc.nextInt();
+//         double c = sc.nextInt();
+
+//         calculateAverage(a, b, c);
+//     }
+// }
+
+// write a function which takes in 2 numbers and returns the greater of those
+// public class shivansh{
+//     public static void greatestOfTwo(int a,int b){
+//         if(a>b){
+//             System.out.println("a is greater thn b");
+//             return;
+//         }
+//         else{
+//             System.out.println("b is greater than a");
+//             return;
+//         }
+//     }
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
+
+//         greatestOfTwo(a, b);
+//     }
+// }
+
+// write a finction that takes in the radius as input and returns the circumference of a circle.
+// public class shivansh{
+//     public static void calculateCircumference(double r){
+//         double circumference = 2*3.14*r;
+//         System.out.println(circumference);
+//         return;
+//     }
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         double r = sc.nextDouble();
+
+//         calculateCircumference(r);
+//     }
+// }
+
+// write the function to print the sum of all odd numbers from 1 to n
+
+// public class shivansh{
+//     public static void calculateSum(int n){
+//         int sum = 0;
+//         for(int i = 1;i<=n;i++){
+//             if(i%2 != 0){
+//                 sum = sum+i;
+                
+                
+//             }
+            
+//         }
+//          System.out.println(sum);
+//         return;
+//     }
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+
+//         calculateSum(n);
+//     }
+// }
+
+//  write a function that takes in age as input and returns if that person is eligible to vote or not.
+
+// public class shivansh{
+//     public static void eligibilityDecider(int age){
+//         if(age<=0){
+//             System.out.println("Invalid input");
+//         }
+//         else if(age>=18){
+//             System.out.println("Eligible to cast vote");
+//         }
+//         else{
+//             System.out.println("Not eligible");
+//         }
+//     }
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         int age = sc.nextInt();
+
+//         eligibilityDecider(age);
+//     }
+// }
+
+// write an infinite loop using do while condition
+// public class shivansh {
+//     public static void main(String[] args) {
+        
+//         // This is an infinite loop using do-while
+//         do {
+//             System.out.println("This will print forever");
+            
+//             // You can add a break condition if you want
+//             // for example: if (someCondition) break;
+
+//         } while (true); // The condition is always true
+//     }
+// }
+
+// // write a program to enter the numbers till the user wants and at the end it should display the count of positive negative and zeros entered
+
+// two numbers are entered by the user , x and n. write a function to find the value of one number raised to the power another x to the power n.
 
         
 
